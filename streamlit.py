987434bdc0f7695e1c735ecb51fc60424f84b606
ayoubamine1 <96729428+ayoubamine1@users.noreply.github.com>
2022-10-10 20:29:12 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -5,7 +5,6 @@ import pickle
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 import pandas as pd 
-data = pd.read_cv('https://github.com/ayoubamine1/streamlit_predition/blob/e6a2e72ab2975752a38a8ba113d40ac727c66697/feelings.csv')
 
 loaded_model = pickle.load(open('model.sav', 'rb'))
 
@@ -24,9 +23,7 @@ inp_species = st.radio(
 if st.button('Make Prediction'):
     result = loaded_model.predict([[age, feel.index(inp_species)+1]])
     st.write(f"Your favore food is : {np.squeeze(result, -1)}")
-    row = {'age':10,'feeling':'without','food':tajine}
-    data.append(row,inplace= True)
-
+   
 
 
 
